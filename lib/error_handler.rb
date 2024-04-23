6fb7a54a472e164542d48b9088f76329d28f8596
Steve Kukla <kukla@adobe.com>
2024-04-22 21:52:09 -0600

    Reset

--- lib/error_handler.rb
+++
@@ -1,7 +0,0 @@
-# lib/error_handler.rb
-class ErrorHandler
-  def self.handle_error(message)
-    puts "Error: #{message}"
-    exit(1)
-  end
-end
--- lib/output_printer.rb
+++
@@ -1,6 +0,0 @@
-# lib/output_printer.rb
-class OutputPrinter
-  def self.print_success(message)
-    puts message
-  end
-end
